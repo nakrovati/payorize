41276941d821c4a92855930f5e4e341abb438214
Daniil Zhuravlev <nakrovati@outlook.com>
2023-10-27 18:55:39 +0400

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [
+    "plugin:perfectionist/recommended-natural",
+    "eslint:recommended",
+    "@nuxtjs/eslint-config-typescript",
+    "prettier",
+  ],
+  root: true,
+};
