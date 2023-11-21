93442ba0fd73a8a97e7ccb8d276d9fcaad674643
Daniil Zhuravlev <nakrovati@outlook.com>
2023-11-21 22:37:33 +0400

    chore: add @nuxt/ui and valibot

---
+++ app.config.ts
@@ -0,0 +1,11 @@
+export default defineAppConfig({
+  ui: {
+    card: {
+      body: {
+        background: "bg-gray-50 dark:bg-gray-800/50",
+      },
+      ring: "ring-1 ring-gray-300 dark:ring-gray-700",
+      divide: "divide-y divide-gray-300 dark:divide-gray-700",
+    },
+  },
+});
