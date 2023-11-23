290b33a9d537ef0c8f65bd5754f397abaeaf861c
Daniil Zhuravlev <nakrovati@outlook.com>
2023-11-23 14:29:18 +0400

    chore: add eslint-plugin-unicorn

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,6 +2,20 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "@nuxtjs/eslint-config-typescript",
+    "plugin:unicorn/recommended",
     "prettier",
+  ],
+  overrides: [
+    {
+      files: ["components/**/*.{ts,vue}"],
+      rules: {
+        "unicorn/filename-case": [
+          "error",
+          {
+            case: "pascalCase",
+          },
+        ],
+      },
+    },
   ],
 };
